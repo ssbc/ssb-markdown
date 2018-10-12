f434ee0084014af6f219a4dd3090b1bdf8e1e249
Christian Bundy <christianbundy@fraction.io>
2018-10-12 10:08:32 -0700

    Fix emoji and refactor for simplicity

--- index.js
+++ index.js
@@ -4,27 +4,51 @@ const ssbref = require('ssb-ref')
 
 const replaceNewlines = (text) => text.replace(/\n+(?!$)/g, ' ')
 
-exports.block = function (text, opts) {
-  const defaults = {
-    imageLink: function (ref) {
-      return '#' + ref
-    },
-    toUrl: function (ref, isImage) {
-      // standard ssb-refs
-      if (ssbref.isFeedId(ref))
-        return '#/profile/'+encodeURIComponent(ref)
-      else if (ssbref.isMsgId(ref))
-        return '#/msg/'+encodeURIComponent(ref)
-      else if (ssbref.isBlobId(ref))
-        return '/'+encodeURIComponent(ref)
-      else if (ref && ref[0] === '#')
-        return '#/channel/'+encodeURIComponent(ref.substr(1))
-      return ''
-    },
-    protocols: ['http', 'https', 'dat']
-  }
+const defaults = {
+  imageLink: function (ref) {
+    return '#' + ref
+  },
+  toUrl: function (ref) {
+    // standard ssb-refs
+    if (ssbref.isFeedId(ref)) {
+      return '#/profile/' + encodeURIComponent(ref)
+    } else if (ssbref.isMsgId(ref)) {
+      return '#/msg/' + encodeURIComponent(ref)
+    } else if (ssbref.isBlobId(ref)) {
+      return '/' + encodeURIComponent(ref)
+    } else if (ref && ref[0] === '#') {
+      return '#/channel/' + encodeURIComponent(ref.substr(1))
+    }
+    return ''
+  },
+  protocols: ['http', 'https', 'ftp'],
+  emoji: (emoji, size) => {
+    size = size || 16
+    const image = {
+      src: `./img/emoji/${emoji}.png`,
+      alt: `:${emoji}:`,
+      title: `:${emoji}:`,
+      class: 'emoji',
+      align: 'absmiddle',
+      height: size,
+      width: size
+    }
 
-  opts = Object.assign({}, defaults, opts)
+    let string = '<img'
+
+    Object.keys(image).forEach(key => {
+      const value = image[key]
+      string += ` ${key}="${value}"`
+    })
+
+    string += '>'
+
+    return string
+  }
+}
+
+exports.block = function (text, opts) {
+  const config = Object.assign({}, defaults, opts)
 
   // init
   const md = new MarkdownIt({ linkify: true })
@@ -33,22 +57,25 @@ exports.block = function (text, opts) {
 
   // protocols
   const defaultProtocols = ['http', 'https', 'ftp']
-  Object.values(opts.protocols).forEach(protocol => {
-    if (!defaultProtocols.includes(protocol))
-      md.linkify.add(protocol + ':', 'http:')
+  const additionalProtocols = ['data', 'dat', 'magnet']
+  Object.values(config.protocols).forEach(protocol => {
+    if (!defaultProtocols.includes(protocol)) { md.linkify.add(protocol + ':', 'http:') }
+  })
+  Object.values(additionalProtocols).forEach(protocol => {
+    if (!config.protocols.includes(protocol)) { md.linkify.add(protocol + ':', 'http:') }
   })
 
   // hashtag
   md.renderer.rules.hashtag_open = function (tokens, idx) {
     var tagName = tokens[idx].content.toLowerCase()
-    const url = opts.toUrl('#' + tagName)
+    const url = config.toUrl('#' + tagName)
     return '<a href="' + url + '">'
   }
 
   // emoji
   md.renderer.rules.emoji = function (token, idx) {
     const emoji = token[idx].markup
-    return `<img src="./img/emoji/${emoji}.png" alt=":${emoji}:" title=":${emoji}:" class="emoji" align="absmiddle" height="16" width="16">`
+    return config.emoji(emoji, 16)
   }
 
   // links
@@ -70,37 +97,41 @@ exports.block = function (text, opts) {
   ['@', '%', '&'].forEach(sigil => {
     md.linkify.add(sigil, {
       validate: function (text, pos, self) {
-        var tail = text.slice(pos);
+        var tail = text.slice(pos)
 
         if (!self.re.sigil) {
           self.re.sigil = new RegExp(
-            '^([a-zA-Z0-9\/=]{44}\.[a-z0-9]+)'
-          );
+            '^([a-zA-Z0-9+/=]{44}.[a-z0-9]+)'
+          )
         }
         if (self.re.sigil.test(tail)) {
-          return tail.match(self.re.sigil)[0].length;
+          return tail.match(self.re.sigil)[0].length
         }
-        return 0;
+        return 0
       },
       normalize: function (match) {
-        match.url = opts.toUrl(match.raw)
+        // shorten the link to 7 characters plus the sigil
+        match.text = match.text.slice(0, 8)
+        // linkify is percent-decoding, so we percent-encode the percent symbol
+        match.text = match.text.replace('%', '%25')
+        match.url = config.toUrl(match.raw)
       }
-    });
+    })
   })
 
   // image
-  md.renderer.rules.image = (tokens, idx, options, env, self) => {
+  md.renderer.rules.image = (tokens, idx) => {
     const token = tokens[idx]
 
     const text = token.attrs[0][1]
     const alt = token.content
-    const url = opts.imageLink(text)
-    const src = opts.toUrl(text, true)
+    const url = config.imageLink(text)
+    const src = config.toUrl(text, true)
 
     // XXX no support for `titleAttr`
     if (alt.startsWith('audio:')) {
       return `<video controls src="${src}" alt="${alt}" />`
-    }  else if (alt.startsWith('video:')) {
+    } else if (alt.startsWith('video:')) {
       return `<video controls src="${src}" alt="${alt}" />`
     } else {
       return `<a href="${url}"><img src="${src}" alt="${alt}"></a>`
@@ -111,6 +142,8 @@ exports.block = function (text, opts) {
 }
 
 exports.inline = function (text, opts) {
+  const config = Object.assign({}, defaults, opts)
+
   // init
   const md = new MarkdownIt()
     .use(require('markdown-it-emoji'))
@@ -118,7 +151,7 @@ exports.inline = function (text, opts) {
   // emoji
   md.renderer.rules.emoji = function (token, idx) {
     const emoji = token[idx].markup
-    return `<img src="./img/emoji/${emoji}.png" alt=":${emoji}:" title=":${emoji}:" class="emoji" align="absmiddle" height="12" width="12">`
+    return config.emoji(emoji, 12)
   }
 
   // links
@@ -137,10 +170,9 @@ exports.inline = function (text, opts) {
   }
 
   // image
-  md.renderer.rules.image = (tokens, idx, options, env, self) => {
+  md.renderer.rules.image = (tokens, idx) => {
     return tokens[idx].content
   }
 
   return replaceNewlines(md.renderInline('' + (text || '')))
 }
-
